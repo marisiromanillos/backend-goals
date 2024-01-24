13cc04fcdbc669ef5aeedcb8e4332e276187025a
Marisi Romanillos <marisiromanillos@Marisis-MBP.lan>
2024-01-24 18:13:58 +0000

    get sql needs improving

--- mySQL/queries.js
+++ mySQL/queries.js
@@ -2,4 +2,10 @@ module.exports = {
   addGoal: (goal, call_to_action) => {
     return `INSERT INTO user_goals (goal, call_to_action) VALUES ('${goal}','${call_to_action}')`;
   },
+  getGoals: () => {
+    return `SELECT id, goal, call_to_action FROM user_goals`;
+  },
+  getGoalsRead: (id) => {
+    return `SELECT id, goal, call_to_action FROM user_goals WHERE id = ?`;
+  },
 };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const helmet = require("helmet");
 const asyncMySql = require("./mySQL/connection");
-const { addGoal } = require("./mySQL/queries");
+const { addGoal, getGoals, getGoalsRead } = require("./mySQL/queries");
 
 // Instance of express
 const app = express();
@@ -17,12 +17,39 @@ app.use(helmet());
 // Convert the body to JSON
 app.use(express.json());
 
-// routes
+// ROUTES
+
+// POST goal route
 app.post("/add_goal", async (req, res) => {
   const { goal, call_to_action } = req.body;
   try {
     const result = await asyncMySql(addGoal(goal, call_to_action));
     res.send({ status: 1, userId: result.insertId });
+  } catch (error) {
+    console.error(error);
+    res.send({ status: 0, reason: error.sqlMessage });
+  }
+});
+
+// GET goals route
+app.get("/goals", async (req, res) => {
+  try {
+    const result = await asyncMySql(getGoals());
+    res.send({ data: result, status: 1 });
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+    res.send({ status: 0, reason: error.sqlMessage });
+  }
+});
+
+// GET goals route READ
+app.get("/goals/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await asyncMySql(getGoalsRead(), [id]);
+    res.send({ data: result, status: 1 });
+    console.log(result);
   } catch (error) {
     console.error(error);
     res.send({ status: 0, reason: error.sqlMessage });
